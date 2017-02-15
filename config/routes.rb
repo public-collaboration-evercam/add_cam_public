e8ca729beac89e59b2db15a7eb32505a044a5c5f
Junaid Farooq <junaid@evercam.io>
2017-02-15 22:14:28 +0500

    Added route for getting data.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'main#index'
+  get '/load_cameras_details' => "main#load_cameras_details"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
