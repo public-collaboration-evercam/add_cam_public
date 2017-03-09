42141f80a2da5f9690c39e55a1d2a8f1c23daa96
Junaid Farooq <junaid@evercam.io>
2017-03-09 11:52:16 +0500

    Ugly fix for name.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -21,7 +21,7 @@ class MainController < ApplicationController
         external_host: get_host_port(ip_port, 0),
         external_http_port: get_host_port(ip_port, 1),
         vendor: "axis",
-        name: '#{info.split(",").last.gsub(/\s+/, "")',
+        name: "#{info.split(',').last.gsub(/\s+/, '')",
         api_id: "#{ENV['USER_API_ID']}",
         api_key: "#{ENV['USER_API_KEY']}"
       }
